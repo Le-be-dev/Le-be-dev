cdfef965e0697e1895892368a8a68536f43cec7b
Your Name <you@example.com>
2022-01-18 16:53:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+pip install django
+
+DJANGO-ADMIN STARTPROJECT TASKFORGE141
+
+cd TASKFORGE141
+
+python manage.py runserver
+
+echo "# Le-be-dev" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Le-be-dev/Le-be-dev.git
+git push -u origin main
+
+git remote add origin https://github.com/Le-be-dev/Le-be-dev.git
+git branch -M main
+git push -u origin main

6b5d121fb63a903551f93762b41065a5c2877aa1
Your Name <you@example.com>
2022-01-18 16:59:21 +0500

    first commit

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Le-be-dev/Le-be-dev.git
-git push -u origin main
+git remote add origin https://github.com/Le-be-dev/djungo.git
 
-git remote add origin https://github.com/Le-be-dev/Le-be-dev.git
+git remote add origin https://github.com/Le-be-dev/djungo.git
 git branch -M main
 git push -u origin main
